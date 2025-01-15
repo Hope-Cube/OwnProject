012f92adc3fc4c2bde67f1b7ad5c40400fa7bcde
Hope-Cube <horvathhunor1@gmail.com>
2025-01-15 09:11:34 +0100

    2025.01.15.2
    Refactor and enhance point handling and SVG generation
    
    Updated using directives in Program.cs to remove unnecessary namespaces and add System.IO and System.Console. Replaced commented-out code for reading points from a file with active code using StreamReader to read points from pointstest.txt as PointD objects. Modified code to rotate a list of points and output to console. Updated StreamWriter to write to test.svg using GenerateSVG with additional parameters. In SVGConverter.cs, changed GenerateSVG to accept PointD objects and added isRounded parameter. Added logic to round coordinates to three decimal places if isRounded is true. Updated SVG path data generation and final SVG string construction to handle new isRounded parameter and correct SVG output.

--- OwnProject/Program.cs
+++ OwnProject/Program.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
+﻿using System.Collections.Generic;
 using System.IO;
-using static OwnProject.PointRotator;
 using static OwnProject.MathUtilities;
+using static OwnProject.PointRotator;
 using static System.Console;
 
 namespace OwnProject
@@ -12,15 +10,16 @@ namespace OwnProject
     {
         static void Main(string[] args)
         {
-            //List<PointF> points = new List<PointF>();
-            //using (StreamReader sR = new StreamReader("pointstest.txt"))
-            //{
-            //    while (!sR.EndOfStream)
-            //    {
-            //        var lineParts = sR.ReadLine().Split(',');
-            //        points.Add(new PointF(float.Parse(lineParts[0]), float.Parse(lineParts[1])));
-            //    }
-            //}
+            List<PointD> points = new List<PointD>();
+            using (StreamReader sR = new StreamReader("pointstest.txt"))
+            {
+                while (!sR.EndOfStream)
+                {
+                    var lineParts = sR.ReadLine().Split(';');
+                    points.Add(new PointD(double.Parse(lineParts[0]), double.Parse(lineParts[1])));
+                }
+            }
+            //Test point list
             //List<PointF> points = new List<PointF>
             //{
             //    new PointF(-2, 2),
@@ -29,14 +28,16 @@ namespace OwnProject
             //    new PointF(1, 4),
             //    new PointF(2, 2)
             //};
-            PointD rotatedPoint = RotatePoint(new PointD(-28.33632505,74.17495355 - 28.33632505), 24.092136856491987, new PointD(74.17495355, 0), false);
-            //WriteLine(string.Join(",\n", rotatedPoints));
+            List<PointD> rotatedPoints = new List<PointD>();
+            PointD center = new PointD(74.17495355, 0);
+            double angleInDegrees = 24.092136856491987 + 180;
+            rotatedPoints = RotatePoints(points, angleInDegrees, center, false);
+            WriteLine(string.Join(",\n", rotatedPoints));
 
-            //using (StreamWriter sW = new StreamWriter("test1.svg"))
-            //{
-            //    sW.Write(SVGConverter.SVGGen(.5, "ddd", "444", 200, rotatedPoints));
-            //}
-            WriteLine(rotatedPoint);
+            using (StreamWriter sW = new StreamWriter("test.svg"))
+            {
+                sW.Write(SVGConverter.GenerateSVG(.5,"444", "ddd", 800, rotatedPoints, true));
+            }
 
             ReadKey(true);
         }
--- OwnProject/SVGConverter.cs
+++ OwnProject/SVGConverter.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Globalization;
 using System.Linq;
+using static OwnProject.MathUtilities;
 
 namespace OwnProject
 {
@@ -20,9 +20,10 @@ namespace OwnProject
         /// <param name="fillColor">The fill color in hexadecimal format (e.g., "00ff00").</param>
         /// <param name="size">The width and height of the SVG viewport.</param>
         /// <param name="points">A list of points defining the path.</param>
+        /// <param name="isRounded">Specifies whether the coordinates should be rounded to 3 decimal places.</param>
         /// <returns>A complete SVG string with the specified path and styling.</returns>
         /// <exception cref="ArgumentException">Thrown when the points list is null or empty.</exception>
-        public static string GenerateSVG(double strokeWidth, string strokeColor, string fillColor, double size, List<PointF> points)
+        public static string GenerateSVG(double strokeWidth, string strokeColor, string fillColor, double size, List<PointD> points, bool isRounded)
         {
             // Validate input
             if (points == null || points.Count == 0)
@@ -34,27 +35,59 @@ namespace OwnProject
             double width = points.Max(p => p.X) - minX;
             double height = points.Max(p => p.Y) - minY;
 
+            // Apply rounding if specified
+            if (isRounded)
+            {
+                minX = Math.Round(minX, 3);
+                minY = Math.Round(minY, 3);
+                width = Math.Round(width, 3);
+                height = Math.Round(height, 3);
+            }
+
+
+            // Build the SVG header
             string svgHeader = $"<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"{minX.ToString(CultureInfo.InvariantCulture)} {minY.ToString(CultureInfo.InvariantCulture)} {width.ToString(CultureInfo.InvariantCulture)} {height.ToString(CultureInfo.InvariantCulture)}\" width=\"{size.ToString(CultureInfo.InvariantCulture)}\" height=\"{size.ToString(CultureInfo.InvariantCulture)}\">";
             string pathStyle = $"<path stroke-width=\"{strokeWidth.ToString(CultureInfo.InvariantCulture)}\" stroke=\"#{strokeColor}\" fill=\"#{fillColor}\" d=\"m";
 
-            // Build the path using relative coordinates
-            string pathData = $"{points[0].X.ToString(CultureInfo.InvariantCulture)} {points[0].Y.ToString(CultureInfo.InvariantCulture)}";
+            // Start path data
+            double startingX = points[0].X;
+            double startingY = points[0].Y;
+
+            //Apply rounding if specified
+            if (isRounded)
+            {
+                startingX = Math.Round(startingX, 3);
+                startingY = Math.Round(startingY, 3);
+            }
+
+            string pathData = $"{startingX.ToString(CultureInfo.InvariantCulture)} {startingY.ToString(CultureInfo.InvariantCulture)}";
             double currentX = points[0].X;
             double currentY = points[0].Y;
 
+            // Generate relative coordinates for the path
             for (int i = 1; i < points.Count; i++)
             {
+                // Calculate deltas
                 double deltaX = points[i].X - currentX;
                 double deltaY = points[i].Y - currentY;
 
+                // Apply rounding if specified
+                if (isRounded)
+                {
+                    deltaX = Math.Round(deltaX, 3);
+                    deltaY = Math.Round(deltaY, 3);
+                }
+
+                // Append relative coordinates
                 pathData += $" {deltaX.ToString(CultureInfo.InvariantCulture)} {deltaY.ToString(CultureInfo.InvariantCulture)}";
 
+                // Update current position
                 currentX = points[i].X;
                 currentY = points[i].Y;
             }
 
             // Close the path and complete the SVG
-            string svg = $"{svgHeader}{pathStyle}{pathData}z\"/></svg>";
+            string svg = $"{svgHeader}{pathStyle.Replace("stroke-width=\"0", "stroke-width=\"")}{pathData.Replace(" -", "-")}z\"/></svg>";
 
             return svg;
         }
